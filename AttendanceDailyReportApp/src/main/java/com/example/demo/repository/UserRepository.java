ede39d13d1200db7be9e88f48c02d643bfdaa5d9
akane <akane@localhost>
2025-06-16 23:44:23 +0900

    Security関係追記

--- AttendanceDailyReportApp/src/main/java/com/example/demo/repository/UserRepository.java
+++ AttendanceDailyReportApp/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public class UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByUsername(String username);
 }
